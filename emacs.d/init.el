d6c3a8d5ed9523547af454df22c3db2139f76e41
mbbogh <mbbogh@users.noreply.github.com>
2019-01-03 14:35:46 +0100

    changed screen back to default - C-\ not possible with terminus started .bashrc_common emacs: ripgrep, flyspell, native relative line number, org configuration change, dired test setup, avy

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,9 @@
-;; at this point in time it contains the complete old configuration
-;; move over into a org file
-;; the file will later only contain machine specific entries
+; prevent Emacs's Easy Customization Interface from writing (custom-set-variables ... into your init file
+(setq custom-file "~/.emacs.d/custom.el")
 
 (setq init-local-file (expand-file-name "init_local.el" user-emacs-directory))
 (when (file-exists-p init-local-file)
   (load init-local-file))
 
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(put 'scroll-left 'disabled nil)
